5ee2ce900d89a453eb6efa1246a98ef81793ba10
Vishak-007 <vishakappukuttan47@gmail.com>
2026-06-21 22:34:35 +0530

    Add majority element algorithm implementation

---
+++ Majority_Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int c=0;
+        for(int num:nums){
+            if(count==0){
+                c=num;
+            }
+            if(num==c) count++;
+            else count--;
+        }
+        return c;
+    }
+}
