403da6186c3dbb769738af2f96092fef4635ca70
Vishak-007 <vishakappukuttan47@gmail.com>
2026-06-21 22:55:53 +0530

    My Optimal Solution for this problem

---
+++ Contains_Dup.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int num:nums){
+            if(!(hs.add(num))){
+                return true;
+            }
+        }
+        return false;
+    }
+}
