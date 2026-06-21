87733b7405afe1d12c82c067db5f149fe1149b45
Vishak-007 <vishakappukuttan47@gmail.com>
2026-06-21 22:53:25 +0530

    Using HashSet to solve this problem

---
+++ Single_Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])){
+                hs.remove(nums[i]);
+            }else{
+                hs.add(nums[i]);
+            }
+        }
+        for(int num:hs) return num;
+        return -1;
+    }
+}
