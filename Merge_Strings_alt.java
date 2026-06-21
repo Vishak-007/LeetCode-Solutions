37cf318fa150348b0434699015f600f02bd965d7
Vishak-007 <vishakappukuttan47@gmail.com>
2026-06-21 23:32:52 +0530

    My solution to merge strings alternativelly

---
+++ Merge_Strings_alt.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0,j=0;
+        String s="";
+        while(i<word1.length()&&j<word2.length()){
+            s+=word1.charAt(i++);
+            s+=word2.charAt(j++);
+        }
+        while(i<word1.length()){
+             s+=word1.charAt(i);
+             i++;
+             }
+        while(j<word2.length()){
+           s+=word2.charAt(j);
+            j++;
+        }
+        return s;
+    }
+}
